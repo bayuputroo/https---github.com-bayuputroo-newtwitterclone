72ecea012268d7656227af2f732a2f9987f48005
bayuputroo <bayuputroous@gmailcom>
2023-07-15 07:51:44 +0700

    persiapan project

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '64687f4076e9c6159670';
+  static const String projectId = '6429387bb206a8ed70af';
+  static const String endPoint = 'https://baas.pasarjepara.com/v1';
+}
